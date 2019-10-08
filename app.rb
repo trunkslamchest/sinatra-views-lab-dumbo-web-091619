b26030482be401a9f684bbe176dc44169dbc36e7
Austin Smith <trunk9slamchest@gmail.com>
2019-10-08 16:01:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 class App < Sinatra::Base
+  
+	configure do
+		set :public_folder, "public"
+		set :views, "views"
+	end
 
 	get '/' do
+
 		erb :index
 	end
 
+	get '/hello' do
 
+		erb :hello
+	end
+
+	get '/goodbye' do
+
+		erb :goodbye
+	end
+
+	get '/date' do
+
+		erb :date
+	end
 end
